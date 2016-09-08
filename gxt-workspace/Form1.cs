4e49d8e54cc6b61058c921f54d9e7f3f836092f5
gxtaillon <gx@taillon.co>
2016-09-08 18:00:55 -0400

    Basic left, right ,up, down movement Seems to bug with maximized windows not returning to their original maximized positions

--- gxt-workspace/Form1.cs
+++ gxt-workspace/Form1.cs
@@ -12,27 +12,62 @@ namespace gxt_workspace
 {
     public partial class Form1 : Form
     {
-        KeyboardHook kbh;
+        List<KeyboardHook> kbhs;
         Movement mvt;
         public Form1()
         {
             InitializeComponent();
-            kbh = new KeyboardHook();
             mvt = new Movement();
-            mvt.Test();
-            // register the event that is fired after the key press.
-            kbh.KeyPressed +=
-                new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
-            // register the control + alt + F12 combination as hot key.
-            kbh.RegisterHotKey(gxt_workspace.ModifierKeys.Control | gxt_workspace.ModifierKeys.Alt,
+            kbhs = new List<KeyboardHook>();
+            // mv left
+            kbhs.Add(new KeyboardHook());
+            kbhs.Last().KeyPressed += new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+            kbhs.Last().RegisterHotKey(gxt_workspace.ModifierKeys.Control | gxt_workspace.ModifierKeys.Alt,
                 Keys.Left);
+
+            // mv right
+            kbhs.Add(new KeyboardHook());
+            kbhs.Last().KeyPressed += new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+            kbhs.Last().RegisterHotKey(gxt_workspace.ModifierKeys.Control | gxt_workspace.ModifierKeys.Alt,
+                Keys.Right);
+
+            // mv up
+            kbhs.Add(new KeyboardHook());
+            kbhs.Last().KeyPressed += new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+            kbhs.Last().RegisterHotKey(gxt_workspace.ModifierKeys.Control | gxt_workspace.ModifierKeys.Alt,
+                Keys.Up);
+
+            // mv down
+            kbhs.Add(new KeyboardHook());
+            kbhs.Last().KeyPressed += new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+            kbhs.Last().RegisterHotKey(gxt_workspace.ModifierKeys.Control | gxt_workspace.ModifierKeys.Alt,
+                Keys.Down);
         }
 
         void hook_KeyPressed(object sender, KeyPressedEventArgs e)
         {
             // show the keys pressed in a label.
-            MessageBox.Show(e.Modifier.ToString() + " + " + e.Key.ToString());
+            //MessageBox.Show(e.Modifier.ToString() + " + " + e.Key.ToString());
+            switch (e.Key)
+            {
+                case Keys.Left:
+                    mvt.Move(Movement.MovementDirection.Left);
+                    break;
+                case Keys.Right:
+                    mvt.Move(Movement.MovementDirection.Right);
+                    break;
+                case Keys.Up:
+                    mvt.Move(Movement.MovementDirection.Up);
+                    break;
+                case Keys.Down:
+                    mvt.Move(Movement.MovementDirection.Down);
+                    break;
+            }
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mvt.Move(Movement.MovementDirection.Left);
+        }
     }
 }
--- gxt-workspace/Movement.cs
+++ gxt-workspace/Movement.cs
@@ -19,52 +19,70 @@ namespace gxt_workspace
         private static extern int EnumWindows(CallBackPtr callPtr, int lPar);
         [DllImport("user32.dll", SetLastError = true)]
         static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, uint wFlags);
 
-        public static bool Report(IntPtr hwnd, int lParam)
+        public static bool Report(IntPtr hWnd, int lParam)
         {
-            var control = Control.FromHandle(hwnd);
             Rectangle rectangle;
-            var visible = IsWindowVisible(hwnd);
+            MovementDirection md = (MovementDirection)lParam;
+            var visible = IsWindowVisible(hWnd);
             if (visible)
             {
-                bool seeable = true;
-                if (control == null)
-                {
-                    RECT rect = new RECT();
-                    GetWindowRect(hwnd, ref rect);
-                    rectangle = new Rectangle(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+                RECT rect = new RECT();
+                GetWindowRect(hWnd, ref rect);
+                rectangle = new Rectangle(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
 
-                    WINDOWPLACEMENT wndpl = new WINDOWPLACEMENT();
-                    wndpl.Lenght = (uint)Marshal.SizeOf(typeof(WINDOWPLACEMENT));
-                    if (!GetWindowPlacement(hwnd, ref wndpl))
-                    {
-                        MessageBox.Show("Bad size!");
-                    }
-                    seeable = wndpl.ShowCmd == (uint)ShowCmd.SW_SHOWMAXIMIZED || wndpl.ShowCmd == (uint)ShowCmd.SW_SHOWNORMAL;
-                }
-                else
+                WINDOWPLACEMENT wndpl = new WINDOWPLACEMENT();
+                wndpl.Lenght = (uint)Marshal.SizeOf(typeof(WINDOWPLACEMENT));
+                if (!GetWindowPlacement(hWnd, ref wndpl))
                 {
-                    rectangle = new Rectangle(control.Left, control.Top, control.Width, control.Height);
+                    MessageBox.Show("Bad size!");
                 }
-
+                var seeable = wndpl.ShowCmd == (uint)ShowCmd.SW_SHOWMAXIMIZED || wndpl.ShowCmd == (uint)ShowCmd.SW_SHOWNORMAL;
+                
                 if (seeable && IsOnScreen(rectangle))
                 {
                     uint pid;
-                    GetWindowThreadProcessId(hwnd, out pid);
-                    MessageBox.Show("Visible Control Window handle is " + hwnd + " at " + rectangle.ToString() + " of pid " + pid);
+                    GetWindowThreadProcessId(hWnd, out pid);
+                    //MessageBox.Show("Visible Control Window handle is " + hWnd + " at " + rectangle.ToString() + " of pid " + pid);
+                    int dLeft = 0, dTop = 0;
+                    switch (md)
+                    {
+                        case MovementDirection.Left:
+                            dLeft = -100;
+                            break;
+                        case MovementDirection.Right:
+                            dLeft = 100;
+                            break;
+                        case MovementDirection.Up:
+                            dTop = -100;
+                            break;
+                        case MovementDirection.Down:
+                            dTop = 100;
+                            break;
+                    }
+                    SetWindowPos(hWnd, 0, rectangle.Left + dLeft, rectangle.Top + dTop, 0, 0, (uint) (SetWindowPosFlags.SWP_NOZORDER | SetWindowPosFlags.SWP_NOSIZE | SetWindowPosFlags.SWP_SHOWWINDOW));
                 }
             }
             return true;
         }
 
-        public void Test()
+        public void Move(MovementDirection md)
         {
 
             // note in other situations, it is important to keep 
             // callBackPtr as a member variable so it doesnt GC while you're calling EnumWindows
 
             callBackPtr = new CallBackPtr(Movement.Report);
-            Movement.EnumWindows(callBackPtr, 0);
+            Movement.EnumWindows(callBackPtr, (int) md);
+        }
+
+        [Flags]
+        public enum MovementDirection : int
+        {
+            Left, Right, Up, Down
         }
 
         // http://stackoverflow.com/a/11655557
@@ -115,6 +133,14 @@ namespace gxt_workspace
             SW_SHOWMAXIMIZED = 3,
             SW_SHOWMINIMIZED = 2,
             SW_SHOWNORMAL = 1
+        }
+
+        [Flags]
+        private enum SetWindowPosFlags : uint
+        {
+            SWP_SHOWWINDOW = 0x0040,
+            SWP_NOZORDER = 0x0004,
+            SWP_NOSIZE = 0x0001
         }
     }
 }
